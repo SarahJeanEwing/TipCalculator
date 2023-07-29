e346c29fda80cea5b2915e9417445836fc9fa07d
SarahJeanEwing <86582382+SarahJeanEwing@users.noreply.github.com>
2023-07-29 09:12:30 -0500

    Update tip_calculator.py

--- tip_calculator.py
+++ tip_calculator.py
@@ -1,6 +1,6 @@
 print("Welcome to the Tip Calculator!")
-bill_total = input("What was the total bill? $")
+bill_total = float(input("What was the total bill? $"))
 tip_percentage = int(input("How much tip would you like to give? 10, 12, or 15? "))/100
-number_of_people = input("How many people to split the bill? ")
-calculated_amount = (float(bill_total)*(1+tip_percentage) / int(number_of_people))
+number_of_people = int(input("How many people to split the bill? "))
+calculated_amount = (bill_total*(1+tip_percentage) / number_of_people)
 print(f"Each person should pay: ${calculated_amount:.2f}")
